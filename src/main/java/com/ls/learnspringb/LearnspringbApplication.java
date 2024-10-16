30db48a4921ec9d00d0e808b012094792dba95ff
mutiawsr <mutiawsr@gmail.com>
2024-10-16 10:38:39 +0700

    [feat] add project

---
+++ src/main/java/com/ls/learnspringb/LearnspringbApplication.java
@@ -0,0 +1,13 @@
+package com.ls.learnspringb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ls/learnspringb/LearnspringbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.learnspringb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
